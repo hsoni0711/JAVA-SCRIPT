24c52842db2fde8a2476d9369b73aaba6e1552e6
hsoni0711 <160158448+hsoni0711@users.noreply.github.com>
2024-02-17 08:40:50 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+//refer document : tc39, mdn 
+"use strict"; // Treat all JS code new version
+
+// alert( 3 + 3) // we are using nodejs not browser
+
+// code readability should be high
+console.log(3 + 3)
+console.log("Himmat")
+
+let name = "Himmat"
+let age = "22"
+let state;
+
+/* Premitive */
+
+// number > 2 to power 53
+// null > standalone value
+// boolean > true or false
+// bigint > big number
+// string > "" or ''
+// symbol > uniqueness
+// undefined > value not define in the variable
+
+/* other dataTypes */
+
+// object
+// typeof
+
+console.log(typeof null)
