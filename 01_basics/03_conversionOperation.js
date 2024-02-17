c3602071996b92b025297ea0016cdf8493435374
hsoni0711 <160158448+hsoni0711@users.noreply.github.com>
2024-02-17 11:34:07 +0000

    Operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -25,8 +25,51 @@ let boolLogin = "Himmat"
 let anyNum = 55
 let StringNum = String(anyNum) 
 
-console.log(StringNum)
-console.log(typeof(StringNum))
+// console.log(StringNum)
+// console.log(typeof(StringNum))
 
 
+/****************************************************** Operations *******************************************************/
 
+let a = 4
+let b = -a
+
+// console.log(b)
+
+// console.log(2+3)
+// console.log(2-3)
+// console.log(2*3)
+// console.log(2**3)
+// console.log(2/3)
+// console.log(2%3)
+
+let str1 = "Hello"
+let str2 = " Himmat"
+let str3 = str1 + str2
+
+// console.log(str3)
+
+// console.log("1" + 2)
+// console.log(2 + "3")
+// console.log(1 + 2 + "3")
+// console.log("1" + 2 + 3)
+
+// console.log((3+4) - 1 * 2 % 1)
+
+//  Not Prefer
+// console.log(+true)
+// console.log(+"")
+
+let n1, n2, n3
+n1 = n2 = n3 = 1+2
+
+let score1 = 100
+++score1;
+
+console.log(score1)
+
+let score2 = 99
+let z = score2++;
+
+console.log(z)
+console.log(score2)
