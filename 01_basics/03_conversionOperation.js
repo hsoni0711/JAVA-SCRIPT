192a238d01c1cf947d355327401f79d563ab4f58
hsoni0711 <160158448+hsoni0711@users.noreply.github.com>
2024-02-17 10:59:32 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,32 @@
+let score = undefined
+
+// console.log(typeof score)
+// console.log(typeof(score))
+
+let numberValue = Number(score)
+
+// console.log(typeof(numberValue)) // number
+// console.log(numberValue) // NaN > Not a Number
+
+// "33" > 33
+// "33abc" > NaN
+// true > 1; false > 0
+// null > 0
+// undefined > NaN
+
+let boolLogin = "Himmat"
+
+//  console.log(Boolean(boolLogin))
+
+// 1 > true; 0 > false
+// "" > false
+// "himmat" > true
+
+let anyNum = 55
+let StringNum = String(anyNum) 
+
+console.log(StringNum)
+console.log(typeof(StringNum))
+
+
+
